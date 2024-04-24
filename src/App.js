d3cf3d30a336ac2a81cb869a6ce0224f41689aa0
Levan <levan.khorava@makingscience.com>
2024-04-24 20:27:05 +0400

    change titl

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import "./index.css";
+
 function App() {
-  return <div className="App">Whiskey day</div>;
+  return (
+    <div className="app">
+      <h1>Whiskey Days</h1>
+      <h2>Coming Soon!</h2>
+    </div>
+  );
 }
 
 export default App;
