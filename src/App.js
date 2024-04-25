7f0805d3b56d3cbd8a6456b83cc9964cbedc552b
Levan <levan.khorava@makingscience.com>
2024-04-25 12:22:52 +0400

    add image and change title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./index.css";
+import photo from "./asssets/logo pasledni.png";
 
 function App() {
   return (
     <div className="app">
+      <img src={photo} width={"300px"} height={"300px"} />
       <h1>Whiskey Days</h1>
       <h2>Coming Soon!</h2>
     </div>
